19b4d5d1ce3da9db44f399b149c1819fb6ce42bc
MirroAi <mirrorai@qq.com>
2020-01-03 13:10:00 +0800

    add case7.py

---
+++ case7.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-03 13:05:09
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-03 13:09:23
+
+# 将一个列表的数据复制到另一个列表中
+
+
+def run():
+
+    l = [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13]
+    L = l
+    print(L)
+
+
+if __name__ == '__main__':
+    run()
