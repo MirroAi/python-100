496010d14c86abc75081366f11b0476a50fb8f3d
MirroAi <mirrorai@qq.com>
2020-01-03 13:45:15 +0800

    update case10.py

--- case10.py
+++ case10.py
@@ -2,7 +2,7 @@
 # @Author: MirrorAi
 # @Date:   2020-01-03 13:37:16
 # @Last Modified by:   MirroAi
-# @Last Modified time: 2020-01-03 13:41:24
+# @Last Modified time: 2020-01-03 13:44:29
 
 
 # 暂停一秒输出，并格式化当前时间
@@ -12,10 +12,9 @@ import time
 
 def run():
 
-    t = time.time()
-    print(t)
+    print(time.asctime(time.localtime(time.time())))
     time.sleep(1)
-    print(time.asctime(time.localtime(t)))
+    print(time.asctime(time.localtime(time.time())))
 
 
 if __name__ == '__main__':
