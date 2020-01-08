b7ec30e588b735934e6ae1c107fd04f7bf9822f6
MirroAi <mirrorai@qq.com>
2020-01-08 21:23:17 +0800

    add case55.py

---
+++ 51-60/case55.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-08 21:19:54
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-08 21:23:03
+
+# 学习使用按位取反 ~
+
+
+def run():
+
+    a = 8
+    b = ~a
+    print("a =", a)
+    print("~a = ", b)
+
+
+if __name__ == "__main__":
+    run()
