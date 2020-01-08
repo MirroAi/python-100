2579dd1cf5aef3880c524b27fd3737a46c5e45ff
MirroAi <mirrorai@qq.com>
2020-01-08 21:42:41 +0800

    add case60.py

---
+++ 51-60/case60.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-08 21:41:01
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-08 21:42:10
+
+# 计算字符串长度
+
+
+def run():
+
+    s = input("请输入一个字符串：")
+    print("%s的长度为%d" % (s, len(s)))
+
+
+if __name__ == "__main__":
+    run()
