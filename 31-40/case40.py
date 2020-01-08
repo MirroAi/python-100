e4c7453107a9f883b9accfae02dc29108f0c7415
MirroAi <mirrorai@qq.com>
2020-01-08 17:58:03 +0800

    add case40.py

---
+++ 31-40/case40.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-08 17:56:27
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-08 17:57:26
+
+# 将一个数组逆序输出
+
+
+def run():
+
+    l = [1, 8, 12, 36, 41, 48, 59, 68, 78, 79, 85, 89, 91, 96]
+
+    print(l)
+    print(l[::-1])
+
+
+if __name__ == "__main__":
+    run()
