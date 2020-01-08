85e673a375a44f81d11278d2e148a29836bb7919
MirroAi <mirrorai@qq.com>
2020-01-08 21:33:40 +0800

    add case56.py

---
+++ 51-60/case56.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-08 21:23:48
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-08 21:33:19
+
+# 画图，学用circle画圆形（未验证，不想安装tk
+
+import turtle
+
+
+def run():
+
+    turtle.circle(20)
+    turtle.mainloop()
+
+
+if __name__ == "__main__":
+    run()
