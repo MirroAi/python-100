62117fabeb2818f82ccb1130492240e737ed8c00
MirroAi <mirrorai@qq.com>
2020-01-08 21:36:51 +0800

    add case57.py

---
+++ 51-60/case57.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-08 21:34:27
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-08 21:36:34
+
+# 画图，学用line画直线
+
+import turtle
+
+
+def run():
+    turtle.penup()
+    turtle.goto(-150, 0)
+    turtle.pendown()
+    turtle.goto(-50, 0)
+
+
+if __name__ == "__main__":
+    run()
