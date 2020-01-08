d5871ce3fe3a1832fcf4826b0289a31ec17e19d5
MirroAi <mirrorai@qq.com>
2020-01-08 21:39:56 +0800

    update case57.py

--- 51-60/case57.py
+++ 51-60/case57.py
@@ -2,7 +2,7 @@
 # @Author: MirrorAi
 # @Date:   2020-01-08 21:34:27
 # @Last Modified by:   MirroAi
-# @Last Modified time: 2020-01-08 21:36:34
+# @Last Modified time: 2020-01-08 21:39:35
 
 # 画图，学用line画直线
 
@@ -14,6 +14,8 @@ def run():
     turtle.goto(-150, 0)
     turtle.pendown()
     turtle.goto(-50, 0)
+
+    turtle.mainloop()
 
 
 if __name__ == "__main__":
