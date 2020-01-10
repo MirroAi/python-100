5565a15fb5d81c4f3c0597ec80c71c152e3c6143
MirroAi <mirrorai@qq.com>
2020-01-10 21:50:13 +0800

    add case72.py, case73.py

---
+++ 71-80/case72.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-10 21:48:38
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-10 21:49:46
+
+# 创建一个链表
+# 略
---
+++ 71-80/case73.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-10 21:49:27
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-10 21:49:42
+
+# 反向输出一个链表
+# 略
