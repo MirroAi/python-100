60d212e8d9eca0962acf62244d8e98dcde247af7
MirroAi <mirrorai@qq.com>
2020-01-12 11:14:55 +0800

    add case77.py

---
+++ 71-80/case77.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-12 11:11:07
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-12 11:13:36
+
+# 循环输出列表
+
+
+def run():
+
+    l = ["a", "b", "c", "d", "e", "f"]
+    for i in l:
+        print(i)
+
+
+if __name__ == "__main__":
+    run()
