c89681f95e9bfa2a90e39cdae861093192cefe59
MirroAi <mirrorai@qq.com>
2020-01-12 11:25:14 +0800

    add case79.py

---
+++ 71-80/case79.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-12 11:21:37
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-12 11:24:31
+
+# 字符串排序
+
+
+def run():
+
+    a = input("请输入字符串：")
+    b = input("请输入字符串：")
+    c = input("请输入字符串：")
+
+    print("字符串排序后为：%s" % sorted([a, b, c]))
+
+
+if __name__ == "__main__":
+    run()
