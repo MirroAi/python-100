4957c4322fe4ed39cca5dd7f2c30bd2cebed32a0
MirroAi <mirrorai@qq.com>
2020-01-20 17:07:31 +0800

    add case84.py

---
+++ 81-90/case84.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-20 17:04:56
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-20 17:06:46
+
+# 连接字符串
+
+
+def run():
+
+    s1 = "abcde"
+    s2 = "hhhhh"
+
+    print(s1+s2)
+    print("%s%s" % (s1, s2))
+
+
+if __name__ == "__main__":
+    run()
