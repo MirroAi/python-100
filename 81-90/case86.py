0d241e203803dc76b3896e4373317f417b55f9c1
MirroAi <mirrorai@qq.com>
2020-01-20 17:20:30 +0800

    add case86.py

---
+++ 81-90/case86.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-20 17:16:59
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-20 17:19:27
+
+# 两个字符串连接程序
+
+
+def function(s1, s2):
+
+    return s1 + s2
+
+
+def run():
+    s1 = input("请输入字符串1：")
+    s2 = input("请输入字符串2：")
+    print(function(s1, s2))
+
+
+if __name__ == "__main__":
+    run()
