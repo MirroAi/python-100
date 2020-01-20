4b26860eb1787f6317dc2bb2f175dcf65bf71b0f
MirroAi <mirrorai@qq.com>
2020-01-20 16:54:44 +0800

    add case82.py

---
+++ 81-90/case82.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Author: MirrorAi
+# @Date:   2020-01-20 16:47:49
+# @Last Modified by:   MirroAi
+# @Last Modified time: 2020-01-20 16:54:12
+
+# 八进制转换为十进制
+
+
+def run():
+
+    x = 111
+    print("八进制 %d 的十进制是 %d" % (x, int(str(x), 8)))
+
+
+if __name__ == "__main__":
+    run()
